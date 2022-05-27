46dcc4dd672569cfe1c183b79550bedc39c16e30
Gustavo Fontenele <gustavoafo171@gmail.com>
2022-05-27 16:04:00 -0300

    exercicios_java01

---
+++ Exercicios/src/gustavo/exercicios/secao04/exercicio01.java
@@ -0,0 +1,9 @@
+package gustavo.exercicios.secao04;
+/*
+Faça um programa que receba dois números e mostre qual deles é o maior.
+*/
+public class exercicio01 {
+	public static void main(String[] args) {
+		
+	}
+}
