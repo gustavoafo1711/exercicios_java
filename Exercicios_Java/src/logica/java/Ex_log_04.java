77c21d501e48029021dcac5641440574c5771521
Gustavo Fontenele <gustavoafo1711@gmail.com>
2022-06-22 20:15:28 -0300

    exercicios logica 04 e 05

---
+++ Exercicios_Java/src/logica/java/Ex_log_04.java
@@ -0,0 +1,30 @@
+package logica.java;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+/*
+Faça um programa que receba um número inteiro e verifique se
+este número é par ou ímpar.
+*/
+public class Ex_log_04 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		try {
+			System.out.println("Informe um número inteiro: ");
+			int num = teclado.nextInt();
+			
+			if(num % 2 == 0) {
+				System.out.println("O número " + num + " é par");
+			} else {
+				System.out.println("O número " + num + " é ímpar");
+			}
+		} catch(InputMismatchException e) {
+			System.out.println("Informe um número inteiro!!!\nOcorreu o seguinte erro: " + e);
+		}
+		teclado.close();
+	}
+
+}
---
+++ Exercicios_Java/src/logica/java/Ex_log_05.java
@@ -0,0 +1,43 @@
+package logica.java;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+/*
+Faça um programa que leia 2 notas de um aluno, verifique se as notas são válidas e
+exiba na tela a média destas notas. Uma nota válida deve ser obrigatoriamente um
+valor entre 0.0 e 10.0, onde caso a nota não possua um valor válido, este fato deve 
+ser informado ao usuário e o programa termina.
+*/
+public class Ex_log_05 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		try {
+			System.out.println("Informe a primeira nota: ");
+			float nota1 = teclado.nextFloat();
+			if (0.0 <= nota1 && nota1 <= 10.0) {
+				System.out.println("Informe a segunda nota: ");
+				float nota2 = teclado.nextFloat();
+				
+				if (nota2 >= 0 && nota2 <=10) {
+					float media = (nota1 + nota2) /2;
+					System.out.println("\nA média do aluno é: " + media);
+				} else {
+					System.out.println("Esse não é um valor válido.");
+				}
+				
+			} else {
+				System.out.println("Esse não é um valor válido.");
+			}
+						
+		} catch (InputMismatchException e) {
+			System.out.println("Esse não é um valor válido: " + e);
+		}
+
+		
+		teclado.close();
+	}
+
+}
