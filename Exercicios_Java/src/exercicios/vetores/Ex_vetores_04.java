d9078116960585ef390a5b1a3fd5dbf09bd84e7d
gustavo <gustavo@gustavo-note>
2022-09-28 15:18:14 -0300

    Ex_vetores_04

---
+++ Exercicios_Java/src/exercicios/vetores/Ex_vetores_04.java
@@ -0,0 +1,37 @@
+package exercicios.vetores;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+04- Faça um programa que leia um vetor de 8 posiçõese em seguida leia também dois
+valores X e Y quaisquer correspondentes a duas posições no vetor. Ao final seu
+programa deverá escrever a soma dos valores encontrados nas respectivas posições X e Y.
+ */
+public class Ex_vetores_04 {
+
+	static Scanner teclado = new Scanner(System.in);
+	static int meuVetor[] = new int[8];
+	static int soma = 0;
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < meuVetor.length; i++) {
+			System.out.println("Insira o " + (i + 1) + (char) (176) + " valor do vetor:");
+			meuVetor[i] = teclado.nextInt();
+		}
+
+		System.out.println("Seu vetor é: " + Arrays.toString(meuVetor));
+
+		System.out.println("\nSomando números aleatórios do vetor.");
+		for (int j = 1; j < 3; j++) {
+			int numAleatorio = (int) (Math.random() * 8);
+			System.out.println("O " + j + (char) (176) + " número do vetor: " + meuVetor[numAleatorio]);
+			soma = soma + meuVetor[numAleatorio];
+		}
+
+		System.out.println("A soma dos números sorteados é: " + soma);
+
+	}
+
+}
